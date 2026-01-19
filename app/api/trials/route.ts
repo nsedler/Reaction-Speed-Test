d80fcb1084e5a2705b0077642e4b1355daf3fcb8
Nate Sedler <nsedler1@gmail.com>
2026-01-18 19:38:35 -0500

    updated prisma bs

--- app/api/trials/route.ts
+++ app/api/trials/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/generated/prisma";
 import { PrismaPg } from '@prisma/adapter-pg';
 
 const adapter = new PrismaPg({ 
